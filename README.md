0259bfef05ddeef12024f131432d6dea4496776a
Momtchil Momtchev <momtchil@momtchev.com>
2020-01-17 20:32:31 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# async/await-compatible Promise-based priority queues
+
+** Bare-bones implementation **
+
+This is an interesting solution to the priority queues problem.
+
+There are other Promise-based queues out there but they are not async/await compatible and do not support priorities.
+
+These can be used to rate-limit expensive external API requests.
+
+The queues keep references to the Promise resolve() function and resolve it from the outside.
+This is a very unusual use of Promises that I find interesting.
+The language specification doesn't make it clear if this is allowed or not, but it seems to work very well.
+
+Example use:
+
+`
+const Queue = require('./queue').Queue;
+/* No more than 2 concurrent tasks with
+ * at least 100ms between two tasks
+ */
+myq = new Queue(2, 100);
+
+....
+
+async function downloadTheUniverse() {
+	/* The third call will wait for the previous two to complete
+	 * plus the time needed to make this at least 100ms
+	 * after the second call
+	 */
+	await myq.wait("MYSOCSECNUM", myPriority);
+
+	/* Do your expensive task */
+	downloadTheInternet();
+
+	/* Signal that we are finished */
+	myq.end("MYSOCSECNUM");
+}
+`
---
+++ queue.js
@@ -0,0 +1,99 @@
+class Queue {
+	/**
+	 * 
+	 * @param {number} _maxConcurrent Number of tasks allowed to run simultaneously
+	 * @param {number} _minCycle Minimum number of milliseconds between two tasks
+	 */
+	constructor(_maxConcurrent, _minCycle) {
+		this.maxConcurrent = _maxConcurrent || 1;
+		this.minCycle = _minCycle || 0;
+		this.queueRunning = [];
+		this.queueWaiting = {};
+		this.lastRun = 0;
+	}
+
+	/** @private */
+	getLastWaiting(prio) {
+		for (let p of Object.keys(this.queueWaiting).sort((a, b) => b - a))
+			if (this.queueWaiting[p] !== undefined && this.queueWaiting[p].length > 0 && p <= prio)
+				return this.queueWaiting[p][this.queueWaiting[p].length - 1];
+		return undefined;
+	}
+
+	/** @private */
+	dequeue(hash, _q) {
+		const q = _q || this.queueRunning;
+		const idx = q.findIndex(x => x.hash === hash);
+		if (idx == -1)
+			throw 'queue desync';
+		const o = q[idx];
+		q.splice(idx, 1);
+		return o;
+	}
+
+	/** @private */
+	getFirstPrioirityWaiting() {
+		for (let p of Object.keys(this.queueWaiting).sort((a, b) => a - b))
+			if (this.queueWaiting[p] !== undefined && this.queueWaiting[p].length > 0)
+				return p;
+		return Infinity;
+	}
+
+	/**
+	 * 
+	 * @param {*} hash Unique hash identifying the task
+	 */
+	end(hash) {
+		const me = this.dequeue(hash, this.queueRunning);
+		/* Resolve my promise to wake up the tasks waiting on me */
+		me.resolve();
+	}
+
+	/**
+	 * 
+	 * @param {*} hash Unique hash identifying the task
+	 * @param {number} priority
+	 * @returns {Promise} Resolved when the task is ready to run
+	 */
+	async wait(hash, priority) {
+		/* Us on the queue */
+		let me = { hash, priority };
+		/* Create priorities on the fly */
+		if (this.queueWaiting[priority] == undefined)
+			this.queueWaiting[priority] = [];
+
+		/* Tasks waiting on us will be waiting on this promise
+		 * We will keep a reference on the resolve function
+		 * and resolve it from the outside */
+		me.promise = new Promise((resolve, reject) => {
+			me.resolve = resolve;
+		});
+		/* Are we allowed to run?
+		* No, if there are already maxConcurrent running tasks
+	    * And no, if there are higher priority tasks waiting
+	    */
+		while (this.queueRunning.length >= this.maxConcurrent || this.getFirstPrioirityWaiting() < priority) {
+			/* Who is the last one in the line? */
+			const lastWaiting = this.getLastWaiting(this.queueWaiting);
+			/* Get in the line */
+			this.queueWaiting[priority].push(me);
+			if (lastWaiting)
+				/* Wait on the last one's promise */
+				await lastWaiting.promise;
+			else
+				/* No one is waiting, wait on all the running promises */
+				await Promise.race(this.queueRunning.map(x => x.promise));
+			/* Get off the line and try to run */
+			this.dequeue(hash, this.queueWaiting[priority]);
+		}
+		/* Wait if it is too soon */
+		if (Date.now() - this.lastRun < this.minCycle)
+			await new Promise((resolve, reject) => setTimeout(resolve, this.minCycle - Date.now() + this.lastRun));
+		/* We are allowed to run */
+		this.queueRunning.push(me);
+	}
+}
+
+module.exports = {
+	Queue
+}
