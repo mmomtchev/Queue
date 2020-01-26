aee43986ba38f7d1ce3cc1e2f33d4abfc6c38e4d
Momtchil Momtchev <momtchil@momtchev.com>
2020-01-26 21:50:23 +0100

    Add stat() and flush()

--- queue.js
+++ queue.js
@@ -36,7 +36,8 @@ class Queue {
 	 * @param {*} hash Unique hash identifying the task
 	 */
 	end(hash) {
-		this.dequeue(hash, this.queueRunning);
+		const me = this.dequeue(hash, this.queueRunning);
+		me.resolve();
 		/* Choose the next task to run and unblock its promise */
 		const q = this.getFirstWaiting();
 		if (q !== undefined) {
@@ -67,14 +68,48 @@ class Queue {
 			/* Get in the line */
 			this.queueWaiting[priority].push(me);
 			await me.promise;
+
 		}
 
 		this.queueRunning.push(me);
-		/* Wait while keeping the running lock if it is too soon */
+		me.promise = new Promise((resolve, reject) => {
+			me.resolve = resolve;
+		});
+		/* Wait if it is too soon */
 		while (Date.now() - this.lastRun < this.minCycle) {
 			await new Promise((resolve, reject) => setTimeout(resolve, this.minCycle - Date.now() + this.lastRun));
 		}
 		this.lastRun = Date.now();
+	}
+
+	/**
+	 * @returns {object} running, waiting, last
+	 */
+	stat() {
+		return {
+			running: this.queueRunning.length,
+			waiting: Object.keys(this.queueWaiting).reduce((t, x) => (t += this.queueWaiting[x].length), 0),
+			last: this.lastRun
+		}
+	}
+
+	/**
+	 * @returns {Promise} returns when the queue is empty
+	 */
+	async flush() {
+		/* Aways wait on the lowest priority in the queue */
+		while (this.stat().waiting > 0) {
+			for (let p of Object.keys(this.queueWaiting).sort((a, b) => b - a)) {
+				const qp = this.queueWaiting[p];
+				if (qp !== undefined && qp.length > 0) {
+					await qp[qp.length - 1].promise;
+				}
+			}
+		}
+		/* And then finish on the running queue */
+		while (this.queueRunning.length > 0) {
+			await Promise.allSettled(this.queueRunning.map(x => x.promise));
+		}
 	}
 }
 
--- test.js
+++ test.js
@@ -5,24 +5,21 @@ async function test(id, pri) {
 	await q.wait(id, pri);
 
 	setTimeout(() => {
-		console.log(id);
+		console.log(id, q.stat());
 		q.end(id);
-		tests--;
 	}, 1000);
 }
 
 let q = new Queue(2, 500);
 
 /* The correct order of execution should be 1, 2, 5, 3, 4 */
-let tests = 5;
 test(Symbol(1), 5);
 test(Symbol(2), 5);
 test(Symbol(3), 5);
 test(Symbol(4), 5);
 test(Symbol(5), 0);
 
-function wait() {
-	if (tests > 0)
-		setTimeout(wait, 1000);
-}
-wait();
+q.flush().then(() => {
+	process.exit(0);
+});
+
